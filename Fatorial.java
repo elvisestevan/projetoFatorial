009e25873a2bbe672a2a8b64f2fafde89094ded0
Elvis Estevan <elvis.estevan65@gmail.com>
2013-10-22 22:59:05 -0200

    Refatoracao da classe TesteFatorial e inclusao de comentario em Fatorial

--- Fatorial.java
+++ Fatorial.java
@@ -1,5 +1,5 @@
 public class Fatorial {
-
+	//Versao recursiva do calculo do fatorial
 	public int calcularFatorial(int n) {
 
 		if (n > 1) {
--- TesteFatorial.java
+++ TesteFatorial.java
@@ -3,8 +3,7 @@ public class TesteFatorial {
 	public static void main (String[] args) {
 
 		Fatorial fatorial = new Fatorial();
-		int resultado = fatorial.calcularFatorial(6);
-		System.out.println("Fatorial de 6: " + resultado);
+		System.out.println(fatorial.calcularFatorial(6));
 
 	}
 
