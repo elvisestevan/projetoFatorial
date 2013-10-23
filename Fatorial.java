ad93cd02d8e87963937af820f9c9267893aac793
Elvis Estevan <elvis.estevan65@gmail.com>
2013-10-22 22:35:53 -0200

    Criação do arquivo principal da aplicacao com método main.

--- Fatorial.java
+++ Fatorial.java
@@ -1,12 +1,6 @@
 public class Fatorial {
 
-	public static void main(String[] args) {
-
-		System.out.println(calcularFatorial(5));		
-
-	}
-
-	public static int calcularFatorial(int n) {
+	public int calcularFatorial(int n) {
 
 		if (n > 1) {
 			return n * calcularFatorial(n - 1);
---
+++ TesteFatorial.java
@@ -0,0 +1,11 @@
+public class TesteFatorial {
+
+	public static void main (String[] args) {
+
+		Fatorial fatorial = new Fatorial();
+		int resultado = fatorial.calcularFatorial(6);
+		System.out.println("Fatorial de 6: " + resultado);
+
+	}
+
+}
