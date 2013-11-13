a0b61b22160b3485dd7b2047f1ed2190ec4fa639
Elvis Estevan <elvis.estevan65@gmail.com>
2013-11-13 08:35:23 -0200

    Calculo do fatorial de 10 em vez de -6 e renomeacao da variavel

--- TesteFatorial.java
+++ TesteFatorial.java
@@ -2,8 +2,8 @@ public class TesteFatorial {
 	//Comentario inserido em TesteFatorial.java
 	public static void main (String[] args) {
 
-		Fatorial fatorial = new Fatorial();
-		System.out.println(fatorial.calcularFatorial(-6));
+		Fatorial f = new Fatorial();
+		System.out.println(f.calcularFatorial(10));
 
 	}
 
